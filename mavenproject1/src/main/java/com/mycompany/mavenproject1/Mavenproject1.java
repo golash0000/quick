2b545d6945c1d95409e8a8036e57e960cb87c0a1
lash0000 <kenbusano2003@gmail.com>
2023-10-24 12:17:33 +0800

    test commit ulet

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Worldijsdkfsidf!");
+        System.out.println("after mo gumawa changes diba save!");
     }
 }
